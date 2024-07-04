3bd73e7b8fbe7cd0b4c209a14edd2625866598bb
Felipe Marques <felipemarquesgg@outlook.com>
2024-07-04 19:35:43 -0300

    criando o projeto

---
+++ src/main/java/com/felipe/ms_authN_authZ_labcenter/MsAuthNAuthZLabcenterApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.ms_authN_authZ_labcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthNAuthZLabcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthNAuthZLabcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipe/ms_authN_authZ_labcenter/MsAuthNAuthZLabcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipe.ms_authN_authZ_labcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthNAuthZLabcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
